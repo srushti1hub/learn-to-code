704f3b6215997b567226161a94af957140f98474
srushti1hub <srushtirtv18@gmail.com>
2021-01-05 10:01:12 +0530

    Day 4

---
+++ C++/class.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Student
+{
+private:
+    int age;
+    int standard;
+    string first_name, last_name;
+
+public:
+    Student()
+    {
+    }
+    ~Student()
+    {
+    }
+    int get_age()
+    {
+        return age;
+    }
+    int get_standard()
+    {
+        return standard;
+    }
+    string get_first_name()
+    {
+        return first_name;
+    }
+    string get_last_name()
+    {
+        return last_name;
+    }
+    void set_age(int i)
+    {
+        age = i;
+    }
+    void set_standard(int i)
+    {
+        standard = i;
+    }
+    void set_first_name(string s)
+    {
+        first_name = s;
+    }
+    void set_last_name(string s)
+    {
+        last_name = s;
+    }
+    string to_string()
+    {
+        return std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
+    }
+};
+int main()
+{
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+
+    return 0;
+}
---
+++ C++/structs.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student
+{
+    unsigned int age;
+    string first_name;
+    string last_name;
+    unsigned int standard;
+};
+
+int main()
+{
+    Student st;
+
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+
+    return 0;
+}
