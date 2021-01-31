33979cf80d7c322727e96e11fabb46b04a42df95
srushti1hub <srushtirtv18@gmail.com>
2021-01-31 10:01:12 +0530

    JS DAY 7

---
+++ JavaScript/Day5/template_literals.js
@@ -0,0 +1,18 @@
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    const [a,p] = expressions;
+    const s1 = (p + Math.sqrt((p*p) - (16*a)))/4 ; 
+    const s2 = (p - Math.sqrt((p*p) - (16*a)))/4 ;
+    return ([s2,s1]); 
+ }
+ 
+ 
+ 
