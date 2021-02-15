de06189e2915c91d510192518d6fb143cfd82bf0
srushti1hub <srushtirtv18@gmail.com>
2021-02-15 10:01:12 +0530

    Input & Output

---
+++ Java/Java-Stdin-and-Stdout-II.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String str = sc.nextLine();
+        str = sc.nextLine();
+        System.out.println("String: "+str);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+i);
+    }
+}
+
