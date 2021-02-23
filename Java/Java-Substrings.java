220ebecde8b8eaa2863fce2aa74757b70492516c
srushti1hub <srushtirtv18@gmail.com>
2021-02-23 10:01:12 +0530

    Strings

---
+++ Java/Java-Substrings.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(str.substring(a,b));
+    }
+}
