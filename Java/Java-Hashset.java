a98ee23828aad96222e6cbbb8b4a8b76de815635
srushti1hub <srushtirtv18@gmail.com>
2021-03-07 10:01:12 +0530

    Hashsets

---
+++ Java/Java-Hashset.java
@@ -0,0 +1,6 @@
+Set<String> sets = new HashSet<>();
+        for(int i=0;i<t;i++)
+        {
+            sets.add(pair_left[i]+" "+pair_right[i]);
+            System.out.println(sets.size());
+        }
